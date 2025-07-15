3890d2806afbd804a7702f90f12875a31740f843
Raumay Aggarwal <aggarwalraumay@gmail.com>
2025-07-16 00:18:27 +0530

    Explained key points and things to remember

---
+++ multimap.cpp
@@ -0,0 +1,17 @@
+/*
+Everything is same as that of map, all functions are same.
+
+EXCEPTIONS AND THINGS TO REMEMBER:
+-it can store duplicate keys (any data type)
+-it can store duplicate values (any daya type)
+-keys are stored in the sorted order
+-works in Log n time
+
+
+we cannot use mp[key] = value; to insert items into the multimap
+
+
+Q. How are the keys sorted if there can be duplicate keys in multimap.
+Ans = They are stored in the order they are inserted
+*/
+
